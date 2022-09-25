b557b95954b7daffce52976110f9cfe748487fdd
Claudio Júnior <claudiojunior9662@gmail.com>
2022-09-25 16:32:46 -0300

    [ADD] aulas 19 e 20

--- Scripts/simplemooc/simplemooc/urls.py
+++ Scripts/simplemooc/simplemooc/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
+    path('cursos/', include('courses.urls', namespace='courses')),
 ]
