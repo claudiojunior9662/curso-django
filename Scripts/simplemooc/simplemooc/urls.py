756c830db4edb0ff711342d7f9bac2a65f2e8715
Claudio Júnior <claudiojunior9662@gmail.com>
2022-09-26 23:00:06 -0300

    [ADD] aula 21

--- Scripts/simplemooc/simplemooc/urls.py
+++ Scripts/simplemooc/simplemooc/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
     path('cursos/', include('courses.urls', namespace='courses')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
