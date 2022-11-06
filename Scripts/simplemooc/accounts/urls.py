fb2bb97e060da172960fa24e5cb95a9d3a648bae
Claudio Júnior <claudiojunior9662@gmail.com>
2022-11-06 17:49:48 -0300

    [ADD] aulas 34 e 35

--- Scripts/simplemooc/accounts/urls.py
+++ Scripts/simplemooc/accounts/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 app_name = 'contas'
 urlpatterns = [
-    path('/entrar', LoginView.as_view(template_name='accounts/login.html'), name='login')
+    path('entrar', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('cadastre-se', views.register, name='register')
 ]
