b7e4df01d2b917af3609c092fda32de51e11b594
Claudio Júnior <claudiojunior9662@gmail.com>
2022-11-15 19:42:17 -0300

    [ADD] aulas 44 e 45

--- Scripts/simplemooc/accounts/views.py
+++ Scripts/simplemooc/accounts/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 
-from .forms import RegisterForm
+from .forms import RegisterForm, EditForm
 
 @login_required
 def dashboard(request):
@@ -14,7 +14,31 @@ def dashboard(request):
 @login_required
 def edit(request):
     template_name='accounts/edit.html'
-    return render(request, template_name)
+    context = {}
+    if request.method == 'POST':
+        form = EditForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            form = EditForm(instance=request.user)
+            context['success'] = True
+    else:
+        form = EditForm(instance=request.user)
+    context['form'] = form
+    return render(request, template_name, context)
+
+@login_required
+def edit_password(request):
+    template_name='accounts/edit_password.html'
+    context = {}
+    if request.method == 'POST':
+        form = PasswordChangeForm(data=request.POST, user=request.user)
+        if form.is_valid:
+            form.save()
+            context['success']=True
+    else:
+        form = PasswordChangeForm(user=request.user)
+    context['form']=form
+    return render(request, template_name, context)
 
 def register(request):
     template_name = 'accounts/register.html'
