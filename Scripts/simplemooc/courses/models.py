e3f065de2fe0b3837880bc9568024da07f31099c
Claudio Júnior <claudiojunior9662@gmail.com>
2023-01-13 21:28:10 -0300

    [ADD] aula 68

--- Scripts/simplemooc/courses/models.py
+++ Scripts/simplemooc/courses/models.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.db import models
 from django.conf import settings
 from core.mail import send_mail_template
@@ -26,6 +27,10 @@ class Course(models.Model):
         from django.urls import reverse
         return reverse('courses:details', kwargs={'slug': self.slug})
 
+    def release_lessons(self):
+        today = timezone.now().date()
+        return self.lessons.filter(release_date__gte=today)
+
     class Meta:
         verbose_name='Curso'
         verbose_name_plural='Cursos'
@@ -43,6 +48,12 @@ class Lesson(models.Model):
 
     def __str__(self):
         return self.name
+
+    def is_available(self):
+        if self.release_date:
+            today = timezone.now().date()
+            return self.release_date >= today
+        return False
 
     class Meta:
         verbose_name='Aula'
--- Scripts/simplemooc/courses/views.py
+++ Scripts/simplemooc/courses/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
-from .models import Course, Enrollment, Comment
+from .models import Course, Enrollment, Comment, Lesson, Material
 from .forms import ContactCourse, CommentForm
 
 from .decorators import enrollment_required
@@ -85,3 +85,51 @@ def show_announcement(request, slug, pk):
     template = 'courses/show_announcement.html'
     context = { 'course': course, 'announcement': announcement, 'form': form }
     return render(request, template, context)
+
+@login_required
+@enrollment_required
+def lessons(request, slug):
+    course = request.course
+    lessons = course.release_lessons()
+    if request.user.is_staff:
+        lessons = course.lessons.all()
+    context = {
+        'course': course,
+        'lessons': lessons
+    }
+    template_name = 'courses/lessons.html'
+    return render(request, template_name, context)
+
+@login_required
+@enrollment_required
+def lesson (request, slug, pk):
+    course = request.course
+    lesson = get_object_or_404(Lesson, pk=pk, course=course)
+    if not request.user.is_staff and not lesson.is_available():
+        messages.error(request, 'Esta aula não está disponível!')
+        return redirect('courses:lessons', slug=course.slug)
+    template_name = 'courses/lesson.html'
+    context = {
+        'course': course,
+        'lesson': lesson
+    }
+    return render(request, template_name, context)
+
+@login_required
+@enrollment_required
+def material(request, slug, pk):
+    course = request.course
+    material = get_object_or_404(Material, pk=pk, lesson__course=course)
+    lesson = material.lesson
+    if not request.user.is_staff and not lesson.is_available():
+        messages.error(request, 'Este material não está disponível!')
+        return redirect('courses:lesson', slug=course.slug, pk=lesson.pk)
+    if not material.is_embedded():
+        return redirect(material.file.url)
+    template_name = 'courses/material.html'
+    context = {
+        'course': course,
+        'lesson': lesson,
+        'material': material
+    }
+    return render(request, template_name, context)
